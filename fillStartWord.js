269e1907ffe26331d8766a832a1c03173de0d308
FILMMY <pratipran.s@mail.kmutt.ac.th>
2022-09-07 22:34:54 +0700

    Update 1

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,6 +1,15 @@
 const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
-  //code here
+  if (word == null || word == undefined){
+    return String(undefined)
+  }
+  else if (startWord.length == word.length){
+    return console.log(word)
+  }else if (startWord.length != word.length){
+    return String(startWord+word)
+  }
 }
+
+// console.log(fillStartWord("first",))
 module.exports = fillStartWord
